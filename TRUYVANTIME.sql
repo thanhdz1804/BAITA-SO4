2cc89c0f6caee9f48b00cc8ffd970c17bea7171a
thanhdz1804 <traiktpro2004@gmail.com>
2025-04-14 18:43:26 +0700

    Add files via upload

---
+++ TRUYVANTIME.sql
@@ -0,0 +1,19 @@
+﻿DECLARE @datetime1 DATETIME = '2025-04-14 07:00:00';
+DECLARE @datetime2 DATETIME = '2025-04-14 10:00:00';
+
+SELECT 
+    gv.HoTen AS [Họ tên GV],
+    mh.TenMH AS [Môn dạy],
+    tkb.GioVao,
+    tkb.GioRa,
+    tkb.NgayHoc
+FROM 
+    TKB tkb
+JOIN 
+    GiaoVien gv ON tkb.MaGV = gv.MaGV
+JOIN 
+    MonHoc mh ON tkb.MaMH = mh.MaMH
+WHERE 
+    -- Ghép Ngày + Giờ để so sánh datetime đầy đủ
+    DATEADD(SECOND, DATEDIFF(SECOND, 0, tkb.GioVao), CAST(tkb.NgayHoc AS DATETIME)) <= @datetime2
+    AND DATEADD(SECOND, DATEDIFF(SECOND, 0, tkb.GioRa), CAST(tkb.NgayHoc AS DATETIME)) >= @datetime1
